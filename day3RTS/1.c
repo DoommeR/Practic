736c687ead98e038f438d8a996af1903a2dfc261
Gayar <success-07@list.ru>
2015-07-12 01:47:32 +0300

    Day_3: broadcast.c Makefile added

--- day3RTS/1.c
+++ day3RTS/1.c
@@ -9,21 +9,21 @@
 int count=0;
 int thread_ids[3];
 pthread_mutex_t count_mutex;
-pthread_cound_t count_threshold_cv;
+pthread_cond_t count_threshold_cv;
 
 void *inc_count(void *t)
 {
 	int i;
 	long my_id = (long)t;
 
-	for (i=0; i< TCOUNT; i++)}
+	for (i=0; i< TCOUNT; i++)
 	{
 		pthread_mutex_lock (&count_mutex);
 		count++;
 
 		if (count == COUNT_LIMIT)
 		{
-			pthread_cond_signal($count_threshold_cv);
+			pthread_cond_signal(&count_threshold_cv);
 			printf("inc_count(): thread %ld. count= %d Threshold reached. \n", my_id, count);
 		}
 		printf("inc_count(): thread %ld. count= %d unlocking mutex /n", my_id, count);
@@ -44,7 +44,7 @@ void *watch_count(void *t)
 	pthread_mutex_lock(&count_mutex);
 	while(count<COUNT_LIMIT)
 	{
-		pthread_count_wait(&count_threshold_cv, &count_mutex);
+		pthread_cond_wait(&count_threshold_cv, &count_mutex);
 		printf("watch_count(): thread %ld condition signal received. \n", my_id);
 		count+=125;
 		printf("watch_count(): thread %ld count now= %d. \n", my_id, count);
@@ -57,20 +57,19 @@ void *watch_count(void *t)
 int main (int argc, char *argv[])
 {
 	int i, rc;
-	long t1=1, t2=2 t3=3;
+	long t1=1, t2=2, t3=3;
 	pthread_t threads[3];
 	pthread_attr_t attr;
 
 	pthread_mutex_init(&count_mutex, NULL);
-
-	pthread_mutex_init(&count_threshold_cv, NULL);
+	pthread_cond_init(&count_threshold_cv, NULL);
 
 	pthread_attr_init(&attr);
-	pthread_attr_setdatachstate(&attr,PTHREAD_CREATE_JOINABLE);
+	pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
+	
 	pthread_create(&threads[0], &attr, watch_count, (void *)t1);
-
 	pthread_create(&threads[1], &attr, inc_count, (void *)t2);
-pthread_create(&threads[2], &attr, inc_count, (void *)t3);
+	pthread_create(&threads[2], &attr, inc_count, (void *)t3);
 
 for(i=0; i<NUM_THREADS; i++)
 {
